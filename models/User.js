a33f04d4593213c12c7b909f2a2cf63d1ed62984
dewerseb <bastien.dewerse@gmail.com>
2020-10-02 16:38:35 +0200

    User model updated

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 let usersSchema = new mongoose.Schema({
     
     username: {
-        type: String
+        type: String,
+        unique : true
     },
     password: {
         type: String
